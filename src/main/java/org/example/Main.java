a4971c85949d32c3eb4973bd3f70285476aaaa58
DD <DD@Asus>
2025-01-30 00:21:21 +0300

    second

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import com.automation.remarks.video.RecorderFactory;
+import com.automation.remarks.video.enums.RecorderType;
+import com.automation.remarks.video.recorder.IVideoRecorder;
+import com.codeborne.selenide.Selenide;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        IVideoRecorder recorder = RecorderFactory.getRecorder(RecorderType.FFMPEG);
+        Selenide.open("https://www.google.ru/");
+        recorder.start();
+        Selenide.sleep(10000);
+        recorder.stopAndSave("test_video");
     }
 }
